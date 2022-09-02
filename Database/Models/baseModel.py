b5112f7c385c226465daaf03786eb874de83c4e2
Julia310 <JPeters92@protonmail.com>
2022-09-02 15:20:40 +0200

    delete code from image branch

--- Database/Models/baseModel.py
+++
@@ -1,9 +0,0 @@
-from peewee import Model
-from Database.dbContext import mysql_db
-
-
-class BaseModel(Model):
-    """A base model that will use our database"""
-
-    class Meta:
-        database = mysql_db
